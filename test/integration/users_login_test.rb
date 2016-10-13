666c43d5936ef3af298952b2ee2fa81d9bf7949a
DucNguyen <20131039@student.hust.edu.vn>
2016-10-13 08:22:56 +0700

    chap9

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   .
@@ -7,20 +7,32 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
-                                          password: "password" } }
+                                          password: 'password' } }
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
-    assert_template "users/show"
+    assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+  
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+     assert_equal FILL_IN, assigns(:user).FILL_IN
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
   end
 end
